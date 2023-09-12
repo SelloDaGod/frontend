1c0d307a87e2d0539794f8dc2e3ace8e5ffc9ef8
SelloDaGod <Marcel.mk1577@gmail.com>
2023-09-12 12:56:56 -0500

    commit

---
+++ src/components/Home.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Container, Row, } from 'react-bootstrap';
+import axios from 'axios';
+import {useState} from 'react';
+export class Home extends Component {
+     
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories: []
+        };
+    }
+  //send request to api for categories
+  //render categories in component
+
+    componentDidMount() {
+       
+        axios.get('https://localhost:7081/categories', { headers: { 'Content-Type': 'application/json' } })
+            .then(response => {
+                this.setState({ categories: response.data });
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+
+    }
+    render() {
+        const { categories } = this.state;
+    return (
+      <div>
+        <h1>Commerce</h1>
+       
+            <Container>
+
+                    {categories.map(category => (
+                        <Row key={category.id}>
+                            <div>{category.name}</div>
+                        </Row>
+                    ))}
+                </Container>
+            </div>
+        );
+    }
+}
+
+
+
